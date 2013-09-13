4ee357e815542ecf757f4cfd605c021ddc8f2ef2
Johnny Graber <jg@jgraber.ch>
2013-09-13 14:15:11 +0200

    Restructured TagCloud

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -5,41 +5,37 @@ class TagCloud
   attr_reader :user,
     :tags, :min, :divisor,
     :tags_90days, :min_90days, :divisor_90days
-  def initialize(user, cut_off)
+
+  def initialize(user, cut_off = nil)
     @user = user
     @cut_off= cut_off
   end
 
-  # TODO: parameterize limit
   def compute
-    levels=10
-
-    params = [sql, user.id]
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
     max, @min = 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @min = [t.count.to_i, @min].min
     }
 
     @divisor = ((max - @min) / levels) + 1
-
-    params = [sql(@cut_off), user.id, @cut_off, @cut_off]
-    @tags_90days = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @min_90days = 0, 0
-    @tags_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @min_90days = [t.count.to_i, @min_90days].min
-    }
-
-    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
-
   end
+
+  def tags
+    unless @tags
+    params = [sql(@cut_off), user.id]
+    if @cut_off
+      params += [@cut_off, @cut_off]
+    end
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    end
+    @tags
+  end
+
 
   private
 
+  # TODO: parameterize limit
   def sql(cut_off = nil)
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
@@ -54,5 +50,9 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
+  end
+
+  def levels
+    10
   end
 end
